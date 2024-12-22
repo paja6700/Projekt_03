9bf030bbd311952190e5576f6e1452fa3f1eca1c
paja6700 <paja6700@gmail.com>
2024-12-22 20:51:12 +0100

    added author informations and imported libraries

---
+++ projekt_03.py
@@ -0,0 +1,14 @@
+"""
+projekt_03.py: třetí projekt do Engeto Online Python Akademie
+
+author: Pavel Křivan
+email: paja6700@gmail.com
+discord: pavel007111
+"""
+
+#import necessary libraries
+import sys
+import requests
+from bs4 import BeautifulSoup
+import csv
+
